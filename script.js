7fc72f6d70bbf9f1c8efe28a890358174492a252
Rens Sibarani <garoga.sehat@gmail.com>
2022-10-17 14:17:25 +0700

    Submit challange Javascript To do list

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Mendeklarasikan sebuah selector
+// Mendeklarasikan selector
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
@@ -9,47 +9,50 @@ todoList.addEventListener("click", deleteCheck);
 
 //function
 function addTodo(event) {
-  //prevent form from submiting
-  event.preventDefault();
+    //agar halaman tidak melakukan reload
+    event.preventDefault();
+    //membuat sebuah inputan kosong
+    const kegiatan = document.createElement("div");
+    kegiatan.classList.add("todo");
+    //menambahkan sebuah kegitan setelah menekan event +
+    const tambahKegiatan = document.createElement("li");
+    tambahKegiatan.innerText = todoInput.value;
+    tambahKegiatan.classList.add("todo-item");
+    kegiatan.appendChild(tambahKegiatan);
 
-  const kegiatan = document.createElement("div");
-  kegiatan.classList.add("todo");
 
-  const tambahKegiatan = document.createElement("li");
-  tambahKegiatan.innerText = todoInput.value;
-  tambahKegiatan.classList.add("todo-item");
-  kegiatan.appendChild(tambahKegiatan);
-  //Tanda cek
-  const tombolSelesai = document.createElement("button");
-  tombolSelesai.innerHTML = '<i class="fas fa-check"> </i>';
-  tombolSelesai.classList.add("complete-btn");
-  kegiatan.appendChild(tombolSelesai);
-  //tombol hapus
-  const tombolHapus = document.createElement("button");
-  tombolHapus.innerHTML = '<i class="fas fa-trash"> </i>';
-  tombolHapus.classList.add("trash-btn");
-  kegiatan.appendChild(tombolHapus);
+    //Mencoret list yang sudah diselesaikan
+    const tombolSelesai = document.createElement("button");
+    tombolSelesai.innerHTML = '<i class="fas fa-check"> </i>';
+    tombolSelesai.classList.add("complete-btn");
+    kegiatan.appendChild(tombolSelesai);
 
-  //append to list
-  todoList.appendChild(kegiatan);
-  //clear input value
-  todoInput.value = "";
+    //menghapus list
+    const tombolHapus = document.createElement("button");
+    tombolHapus.innerHTML = '<i class="fas fa-trash"> </i>';
+    tombolHapus.classList.add("trash-btn");
+    kegiatan.appendChild(tombolHapus);
+
+    //append to list
+    todoList.appendChild(kegiatan);
+    //clear input value
+    todoInput.value = "";
 }
 
 function deleteCheck(e) {
-  const item = e.target;
-  //function untuk menghapus list
-  if (item.classList[0] === "trash-btn") {
-    const todo = item.parentElement;
-    //animasi
-    todo.classList.add("fall");
-    todo.addEventListener("transitionend", function () {
-      todo.remove();
-    });
-  }
-  //animasi yang akan muncul ketika tombol selesai di klik
-  if (item.classList[0] === "complete-btn") {
-    const todo = item.parentElement;
-    todo.classList.toggle("completed");
-  }
+    const item = e.target;
+    //untuk menghapus list
+    if (item.classList[0] === "trash-btn") {
+        const todo = item.parentElement;
+        //animasi menghapus
+        todo.classList.add("fall");
+        todo.addEventListener("transitionend", function() {
+            todo.remove();
+        });
+    }
+    //ketika tombol selesai di klik akan ada animasi
+    if (item.classList[0] === "complete-btn") {
+        const todo = item.parentElement;
+        todo.classList.toggle("completed");
+    }
 }
